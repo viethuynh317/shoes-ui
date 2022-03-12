fa23a459bb28664b05fe18aa3961604fe8a68d9a
Viet Huynh <huynhvanviet317@gmail.com>
2022-03-12 19:12:20 +0700

    commit

--- src/helper/socketIo.js
+++ src/helper/socketIo.js
@@ -1,4 +1,4 @@
 import socketIOClient from 'socket.io-client';
-const ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT;
+// const ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT;
 
-export const socket = socketIOClient(ENDPOINT);
+export const socket = socketIOClient('https://v-shoes-api.herokuapp.com');
